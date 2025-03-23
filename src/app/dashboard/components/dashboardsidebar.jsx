65da54d491a9ad249ccb740970b0a956fd60fcde
bmdx26 <poseidonx26@gmail.com>
2025-03-24 02:16:46 +0530

    finance dash updatess

--- src/app/dashboard/components/dashboardsidebar.jsx
+++ src/app/dashboard/components/dashboardsidebar.jsx
@@ -7,7 +7,7 @@ function DashboardSidebar() {
   return (
     <div className="flex flex-col h-full">
       <ul className="flex-grow">
-        <li className="pt-4"><Link href="/dashboard" className="block p-2 hover:bg-gray-800 rounded">Home</Link></li>
+        <li className="pt-12"><Link href="/dashboard" className="block p-2 hover:bg-gray-800 rounded">Home</Link></li>
         <li><Link href="/dashboard/finance" className="block p-2 hover:bg-gray-800 rounded">Finance</Link></li>
         <li><Link href="/dashboard/inventory" className="block p-2 hover:bg-gray-800 rounded">Inventory</Link></li>
       </ul>
